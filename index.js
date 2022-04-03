28f6703cfca7697d150ce117c80d33d70bb2c206
= <=>
2022-04-03 15:10:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/' , (req , res) => {
+    res.status(200).json({
+        status: true ,
+        message: "Hello World"
+    });
+})
+
+app.listen(3000 , () => {
+    console.log("[Server] start on port 3000");
+});
